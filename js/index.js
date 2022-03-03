af43e174d3617907093b1f8dfb89a1bd8c09efc4
EBRUSTECH <omoyibo.victory@gmail.com>
2022-03-03 21:56:22 +0100

    my short profile

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(function() {
+
+    setTimeout(() => {
+        $("#profile").text(" A junior full stack website developer who is ready to upskill and deploy newest technology in building real life projects. A very good team player and tech savvy, looking for a reasonable work that challenges the mind, foster innovation and contributes to national development. A good researcher with sound knowledge of html, css, JavaScript, jQuery, Vue Js, MySQL, PHP, and Laravel.");
+    }, 1000);
+
+    setInterval(() => {
+        $("#ready").text("I'm fully ready for website development projects, remote jobs and iternship to develop my skills, learn new technologies and build awesome products that will interest client(s), and as well user friendly. You can call or send an email let's get started  ")
+    }, 2000)
+
+ } )
