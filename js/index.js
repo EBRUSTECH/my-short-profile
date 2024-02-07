46d881c915ad2c5cefca599cb88b20d5cbfc28ef
DECAGON <DECAGON@DESKTOP-SC62UQU>
2024-02-07 19:03:13 +0100

    updated my profile

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 $(function() {
 
     setTimeout(() => {
-        $("#profile").text(" A junior full stack website developer who is ready to upskill and deploy newest technology in building real life projects. A very good team player and tech savvy, looking for a reasonable work that challenges the mind, foster innovation and contributes to national development. A good researcher with sound knowledge of html, css, JavaScript, jQuery, Vue Js, MySQL, PHP, and Laravel.");
+        $("#profile").text(" I am a full stack software engineer with three years of experience in software development life cycle, analysis and design. My area of expertise includes core front end development using HTML, CSS, Java- Script, j-Query, and VueJS and back end development using Microsoft SQL Server, MySQL, .Net Core, and PHP/Laravel. I am a very good team player and tech savvy, looking for a reputable company to grow and gain hands-on expertise on applicable skills.");
     }, 1000);
 
     setInterval(() => {
-        $("#ready").text("I'm fully ready for website development projects, remote jobs and iternship to develop my skills, learn new technologies and build awesome products that will interest client(s), and as well user friendly. You can call or send an email let's get started  ")
+        $("#ready").text("I'm fully ready for website development projects, remote jobs, hybrid and full-time role to build awesome products that will interest client(s), and as well user friendly. You can call or send an email let's get started  ")
     }, 2000)
 
  } )
